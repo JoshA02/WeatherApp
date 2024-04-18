2c895933f13e5bc94b9cbe4ce9b89b5d4e0142e8
Josh <joshuav2012@live.co.uk>
2024-04-18 22:00:58 +0100

    Added unit support for daily and hourly data (API::getDaysFromLocationAndRange)

--- src/API.cpp
+++ src/API.cpp
@@ -72,12 +72,18 @@ std::string API::getCurrentDataFromLocation(Location& loc)
 
 // Returns data for each day requested, including hourly data for each day.
 std::vector<dayData> API::getDaysFromLocationAndRange(Location& loc, Date startDate, Date endDate, std::list<std::string> dailyKeysToInclude, std::list<std::string> hourlyKeysToInclude, std::string urlPrefix){
-
-    //WeatherUnits units = getUnits();
-    // TODO: Add units and more daily data options
     using namespace std;
 
-    string urlSuffix = format("?latitude={}&longitude={}&hourly=temperature_2m,relative_humidity_2m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&start_date={}&end_date={}", std::to_string(loc.getCoords().latitude), std::to_string(loc.getCoords().longitude), startDate.toString("YYYY-MM-DD"), endDate.toString("YYYY-MM-DD"));
+    WeatherUnits units = getUnits();
+
+    string urlLatLong = format("latitude={}&longitude={}", std::to_string(loc.getCoords().latitude), std::to_string(loc.getCoords().longitude));
+    string urlHourlyDataPoints = format("hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_to_1cm,soil_moisture_1_to_3cm,soil_moisture_3_to_9cm,soil_moisture_9_to_27cm,soil_moisture_27_to_81cm");
+    string urlDailyDataPoints = format("daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,rain_sum");
+    string urlOptions = format("temperature_unit={}&wind_speed_unit={}&precipitation_unit={}&timezone=Europe%2FLondon", units.tempUnit, units.windSpeedUnit, units.precipUnit);
+    string urlStartDate = format("start_date={}", startDate.toString("YYYY-MM-DD"));
+    string urlEndDate = format("end_date={}", endDate.toString("YYYY-MM-DD"));
+
+    string urlSuffix = ("?" + urlLatLong + "&" + urlHourlyDataPoints + "&" + urlDailyDataPoints + "&" + urlOptions + "&" + urlStartDate + "&" + urlEndDate);
 	std::string url = urlPrefix + urlSuffix;
 
     auto jsonString = get_response(url);
